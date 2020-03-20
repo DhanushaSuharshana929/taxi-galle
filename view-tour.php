b99d7b43dda9c3d8070d103566c18ef09ed0f071
Tharushi07 <tharushi.synotec@gmail.com>
2020-03-20 12:33:35 +0530

    #8 Click tour box then go to the tours (one day,round) page

---
+++ view-tour.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+        <title>Tour Arrangement | Taxi Galle</title>
+        <meta content="" name="author">
+        <meta name="description" content="Holiday is a dream and we know it We, the team BONVOYAGELANKA travel crew is privileged to show you and to take you around the most beautiful island in the world..">
+        <meta content="About Bonvoyage Lanka, Bonvoyage Lanka, About Travel, About tour Web Sites, Travel, Tour, Sri Lanka, Activities in Sri Lanka" name="keywords">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+        <!-- favicon -->
+        <link href="img/favicon1.png" rel="icon" sizes="32x32" type="image/png">
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- font themify CSS -->
+        <link rel="stylesheet" href="css/themify-icons.css">
+        <!-- font awesome CSS -->
+        <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+        <!-- date picker CSS -->
+        <link href="css/datepicker.min.css" rel="stylesheet">
+        <!-- revolution slider css -->
+        <link rel="stylesheet" type="text/css" href="css/fullwidth.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+        <link rel="stylesheet" href="css/rev-settings.css" type="text/css">
+        <!-- on3step CSS -->
+        <link href="css/animated-on3step.css" rel="stylesheet">
+        <link href="css/owl.carousel.css" rel="stylesheet">
+        <link href="css/owl.theme.css" rel="stylesheet">
+        <link href="css/on3step-style.css" rel="stylesheet">
+        <link href="css/queries-on3step.css" media="all" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+
+        <!-- preloader -->
+        <div class="bg-preloader-white"></div>
+        <div class="preloader-white">
+            <div class="mainpreloader">
+                <span></span>
+            </div>
+        </div>
+        <!-- preloader end -->
+
+        <!-- content wraper -->
+        <div class="content-wrapper">
+
+            <!-- Header -->
+            <?php
+            include 'header.php';
+            ?>
+            <!-- /Header -->
+
+            <!-- subheader -->
+            <section id="subheader">
+                <div class="container-fluid m-5-hor">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="big-heading">
+                                Tour Arrangement
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- subheader end -->
+
+            <!--view tour arrangement-->
+            <section id="content-commerce">
+                <div class="container-fluid m-5-hor">
+                    <div class="container-fluid">
+                    <div class="row">
+                        
+
+                        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <div class="feature-1 gal-home1">
+                                <div class="cont-img">
+                                    <img alt="img-cont" class="img-responsive" src="img/service-1.jpg">
+                                </div>
+                                <div class="cont-detail">
+                                    <h3 class="big-heading"><span class="color">One Day Tours</span></h3>
+                                    <p class="max-char">We offer numerous luxury transportation services to corporate groups, 
+                                        executives, VIPs and leisure travellers to travel around Sri Lanka. We have well-reviewed 
+                                        Sri Lanka tour...</p>
+                                    <div class="star-content color">
+                                    </div>
+                                    <div class="btn-center">
+                                        <a href="view-one-day-tour-packages.php" class="btn-content">
+                                        Read More
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <div class="feature-1 gal-home2">
+                                <div class="cont-img">
+                                    <img alt="img-cont" class="img-responsive" src="img/service-2.jpg">
+                                </div>
+                                <div class="cont-detail">
+                                    <h3 class="big-heading"><span class="color">Round Tours</span></h3>
+                                    <p class="max-char">We offer numerous luxury transportation services to corporate groups, 
+                                        executives, VIPs and leisure travellers to travel around Sri Lanka. We have well-reviewed 
+                                        Sri Lanka tour...</p>
+                                    <div class="star-content color">
+                                    </div>
+                                    <div class="btn-center">
+                                        <a href="tour-packages.php" class="btn-content">
+                                        Read More
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       </div>
+                    </div>
+                        
+                </div>
+                </div>
+            </section>
+            <!--view tour arrangement end-->
+
+
+            <!-- footer -->
+            <?php
+            include 'footer.php';
+            ?>
+            <!-- footer end -->
+
+
+            <!-- ScrolltoTop -->
+            <div id="totop" class="init">
+                <span class="ti-angle-up"></span>
+            </div>  
+
+        </div>
+        <!-- content wraper end --> 
+
+        <!-- plugin JS -->
+        <script src="plugin/pluginson3step.js"></script> 
+        <script src="plugin/bootstrap.min.js"></script>
+        <script src='plugin/bootstrap-datepicker.min.js'></script>
+        <script src="plugin/sticky.js"></script> 
+        <!-- slider revolution  -->
+        <script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+        <script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+        <!-- on3step JS -->
+        <script src="js/on3step.js"></script>
+        <script src="js/plugin-set.js"></script>
+    </body>
+</html>
